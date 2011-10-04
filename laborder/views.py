6f75e849b81d47ed974384ff645b7f8acd9620b5
annndrey <gontchar@gmail.com>
2011-10-04 16:41:01 +0400

    Сделана примитивная авторизация с обработкой ошибок и перенаправлением при успешном входе.

--- laborder/views.py
+++ laborder/views.py
@@ -8,13 +8,31 @@ from django.core.mail import send_mail
 from django.core.context_processors import csrf
 from django.http import HttpResponseRedirect
 from laborder.orders.models import Stuff
+from django.contrib import auth
 
-def main(request):
-    return render_to_response("base.html", {'page_name':u'Начальная страница'})
+def main(request, template_name='login.html'):
+    c = {}
+    c.update(csrf(request))
+    if 'username' in request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+        # Правильный пароль и пользователь "активен"
+            auth.login(request, user)
+        # Перенаправление на "правильную" страницу
+            return HttpResponseRedirect("/wishes")
+        else:
+        # Отображение страницы с ошибкой
+            c.update({'login_error':True})
+            return render_to_response("login.html", c)
+    else:
+        return render_to_response('login.html', c)
+        
 
 def wishes(request):
     objs = Stuff.objects.order_by('stgroup')
-    return render_to_response("base.html", {'stuff':objs, 'page_name':u'Список оборудования'})
+    return render_to_response("base.html", {'stuff':objs, 'page_name':u'Список оборудования', 'username':request.user.username})
 
 def hello(request):
     lst = request.META.items()
